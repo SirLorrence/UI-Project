1f4739076a7819b3f52eb3195b9711a785f3cc79
Laurence Sadler <33729843+lvlzro@users.noreply.github.com>
2020-10-08 15:09:31 -0700

    Meta Vignette added

--- Assets/UI_Manager.cs
+++ Assets/UI_Manager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 using UnityEngine.UI;
 
 public class UI_Manager : MonoBehaviour
@@ -9,24 +10,41 @@ public class UI_Manager : MonoBehaviour
     public Slider staminaBar;
     public Slider healthBar;
 
+    public PostProcessProfile playerVignette;
+
     private static UI_Manager _instance;
 
     public static UI_Manager Instance
     {
         get
         {
-            if(_instance == null) Debug.Log("No game manager in scene");
+            if (_instance == null) Debug.Log("No game manager in scene");
             return _instance;
-        }  
+        }
     }
+
     private void Awake()
     {
         _instance = this;
-    }  
+    }
+
+    private void Update()
+    {
+        DamageVignette();
+    }
+
+    #region Player Stats
+
     public void SetStaminaBar(float maxValue) => staminaBar.maxValue = maxValue;
     public void UpdateStaminaBar(float sprintValue) => staminaBar.value = sprintValue;
     public void SetHealthBar(float maxValue) => healthBar.maxValue = maxValue;
-      public void UpdateHealthBar(float sprintValue) => healthBar.value = sprintValue;
-    
-    
+    public void UpdateHealthBar(float sprintValue) => healthBar.value = sprintValue;
+
+    #endregion
+
+    private void DamageVignette()
+    {
+        var temp = 1 - healthBar.value / 100;
+        playerVignette.GetSetting<Vignette>().intensity.value = temp;
+    }
 }
